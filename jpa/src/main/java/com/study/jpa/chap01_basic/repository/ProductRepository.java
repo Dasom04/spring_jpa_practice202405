d649f7d00036e620cae83a421e5e048ec402ad52
Dasom04 <slz_952@naver.com>
2024-05-13 21:30:49 +0900

    파일 위치 수정 완료

--- jpa/src/main/java/com/study/jpa/chap01_basic/repository/ProductRepository.java
+++ jpa/src/main/java/com/study/jpa/chap01_basic/repository/ProductRepository.java
@@ -1,6 +1,6 @@
 package com.study.jpa.chap01_basic.repository;
 
-import com.study.jpa.entity.Product;
+import com.study.jpa.chap01_basic.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> { // 엔터티의 타입, 프라이머리키의 타입
