e38170286c8e4dfe08737db10ce14efe7acd4427
Dasom04 <slz_952@naver.com>
2024-05-14 15:06:56 +0900

    JPA 연관관계 맵핑(양방향), FETCH JOIN(N+1 문제)

--- jpa/src/main/java/com/study/jpa/chap04_relation/retository/DepartmentRepository.java
+++ jpa/src/main/java/com/study/jpa/chap04_relation/retository/DepartmentRepository.java
@@ -2,6 +2,16 @@ package com.study.jpa.chap04_relation.retository;
 
 import com.study.jpa.chap04_relation.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface DepartmentRepository extends JpaRepository<Department,Long> {
+
+    @Query("SELECT d FROM Department d JOIN FETCH d.employees") // 엔터티 클래스 이름으로 써줘야함
+    List<Department> findAllIncludesEmployees();
+
+
+
+
 }
