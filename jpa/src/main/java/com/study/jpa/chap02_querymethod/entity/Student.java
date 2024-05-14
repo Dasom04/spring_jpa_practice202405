4755eaf387ca516c412678ed00fb0975f08b363a
Dasom04 <slz_952@naver.com>
2024-05-14 10:48:56 +0900

    JPA Repository 연습 (JPA Pagination)

--- jpa/src/main/java/com/study/jpa/chap02_querymethod/entity/Student.java
+++ jpa/src/main/java/com/study/jpa/chap02_querymethod/entity/Student.java
@@ -2,7 +2,6 @@ package com.study.jpa.chap02_querymethod.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.GenericGenerator;
 
 @Setter // 실무적 측면에서 setter는 신중하게 선택할 것. (직접 변경하고자 하는 필드만 따로 설정하는 경우가 많음.)
 // 안정성을 위해, 혹은 setter를 직접 선언하려면 안씀
@@ -17,8 +16,9 @@ public class Student {
 
     @Id
     @Column(name = "stu_id")
-    @GeneratedValue(generator = "uid")
-    @GenericGenerator(name = "uid", strategy = "uuid")
+//    @GeneratedValue(generator = "uid")
+//    @GenericGenerator(name = "uid", strategy = "uuid") (과거)
+    @GeneratedValue(strategy = GenerationType.UUID) // Spring Boot3에서 JPA를 사용할 때 권장하는 문법. (최신)
     private String id;
 
     @Column(name = "stu_name" , nullable = false)
